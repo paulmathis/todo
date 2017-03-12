f3b4acfb52e7dcb7bc0136460fb19b6ea0a24478
Paul Mathis <paul.mathis@gmail.com>
2017-03-12 18:21:26 -0500

    Added ability to remove added tasks

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 window.onload = function(){
+    //variable Assigning
     var addTaskButton = document.getElementById("addTaskButton");
     var addTaskInput = document.getElementById("addTaskInput");
 
+    //Show task text input from button click
     addTaskButton.onclick = function() {
         addTaskInput.classList.toggle("hidden");
         this.classList.toggle("hidden");
     };
-
-    /*var taskBar = document.querySelectorAll("div.task.bar");
-    console.log(taskBar);
-    taskBar.onclick = function() {
-        console.log("test");
-    };*/
-    
+     
+    //Take text input value and add as a new task
     addTaskInput.addEventListener("keydown", function(e){
-        if (event.keyCode == 13) {
+        if (event.keyCode == 13) {        
             if (this.value != "") {
                 addTask(this.value);
             }
@@ -22,15 +19,23 @@ window.onload = function(){
             this.classList.toggle("hidden")
             addTaskButton.classList.toggle("hidden");
         }
-    }); 
+    });   
 };
 
+//Add Task to List
 function addTask(val) {
-    var tasks = document.getElementById("tasks");
-    var taskBar=document.createElement("div");
-    taskBar.classList.add("task", "bar");
-    var newTask = tasks.appendChild(taskBar);
-    newTask.innerHTML = "<div class='circle'>\
-                        </div><span>"+val+"</span>\
-                        <div class='close'>x</div>";
-}
+        var tasks = document.getElementById("tasks");
+        var taskBar=document.createElement("div");
+        taskBar.classList.add("task", "bar");
+        var newTask = tasks.appendChild(taskBar);
+        newTask.innerHTML = "<div class='circle'>\
+                            </div><span>"+val+"</span>\
+                            <div class='close' onclick='removeParent(this)'>x</div>";
+    }
+
+//Remove Parrent Element   
+function removeParent(ele) {
+    ele.parentElement.remove();
+};
+
+
