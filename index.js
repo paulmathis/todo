52f517d939302fdd4fea34810de3e5f6f93dcaf0
Paul Mathis <paul.mathis@gmail.com>
2017-03-12 12:32:13 -0500

    Added task adding functionality

--- index.js
+++ index.js
@@ -7,5 +7,30 @@ window.onload = function(){
         this.classList.toggle("hidden");
     };
 
-
+    /*var taskBar = document.querySelectorAll("div.task.bar");
+    console.log(taskBar);
+    taskBar.onclick = function() {
+        console.log("test");
+    };*/
+    
+    addTaskInput.addEventListener("keydown", function(e){
+        if (event.keyCode == 13) {
+            if (this.value != "") {
+                addTask(this.value);
+            }
+            this.value = "";
+            this.classList.toggle("hidden")
+            addTaskButton.classList.toggle("hidden");
+        }
+    }); 
 };
+
+function addTask(val) {
+    var tasks = document.getElementById("tasks");
+    var taskBar=document.createElement("div");
+    taskBar.classList.add("task", "bar");
+    var newTask = tasks.appendChild(taskBar);
+    newTask.innerHTML = "<div class='circle'>\
+                        </div><span>"+val+"</span>\
+                        <div class='close'>x</div>";
+}
