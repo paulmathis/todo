9e0da5fc74cd9c4d32392aa852100a72e7fc7a5c
Paul Mathis <paul.mathis@gmail.com>
2017-03-11 20:53:51 -0600

    Added task input shown on click of + sign

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("test");
+window.onload = function(){
+    var addTaskButton = document.getElementById("addTaskButton");
+    var addTaskInput = document.getElementById("addTaskInput");
+
+    addTaskButton.onclick = function() {
+        addTaskInput.classList.toggle("hidden");
+        this.classList.toggle("hidden");
+    };
+
+
+};
