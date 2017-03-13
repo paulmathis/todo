04a55bcc8f5039bba0891f9d119258f1f1843c31
Paul Mathis <paul.mathis@gmail.com>
2017-03-12 22:35:55 -0500

    Added toggle of tasks views

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 window.onload = function(){
-    
-   //Show task text input from button click
-    $("#addTaskButton").onclick = toggleFooter;
-     
+
     //Take text input value and add as a new task
     $("#addTaskInput").addEventListener("keydown", function(e){
         if (event.keyCode == 13) {        
@@ -26,32 +23,60 @@ function addTask(val) {
                         </div>\
                         <span>"+val+"</span>\
                         <div class='close' onclick='removeParent(this)'>x</div>";
-};
+}
 
 //Add Check Mark
 function addCheck(ele) {
     var checkMark = ele.querySelector("i");
     checkMark.classList.toggle("hidden");
-};
+    ele.parentElement.classList.toggle("checked");
+}
 
 //Remove Parrent Element   
 function removeParent(ele) {
     ele.parentElement.remove();
-};
+}
 
 //jquery like selector
 function $(selector){
     return document.querySelector(selector);
-};
+}
 
 //First argument is the class you want to toggle, then as many selectors that you want to affect
 function toggleClass(className) {
     for (var i=1; i<arguments.length; i++) {
         $(arguments[i]).classList.toggle(className);
     }
-};
+}
 
 //Toggle footer elmenet to be hidden or unhidden
 function toggleFooter() {
     toggleClass("hidden", "#addTaskButton", "#addTaskInput", "#showAll", "#showComplete", "#showIncomplete");
-};
+}
+
+//Toggle view between all tasks, completed tasks, and incomplete tasks
+function show(pick) {
+    var tasks = document.querySelectorAll(".task");
+    for (i=0; i<tasks.length; i++) {
+        var task = tasks[i];
+        switch (pick) {
+            case "all":
+                task.classList.remove("hidden");
+                break;
+            case "complete":
+                if(task.classList.contains("checked")){
+                    task.classList.remove("hidden");
+                } else {
+                    task.classList.add("hidden");
+                }
+                break;
+            case "incomplete":
+                if(!task.classList.contains("checked")){
+                    task.classList.remove("hidden");
+                } else {
+                    task.classList.add("hidden");
+                }
+                break;
+        };        
+    } 
+}
