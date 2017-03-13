65ba97ba24db385a5bc12afab1139e3a120dca1f
Paul Mathis <paul.mathis@gmail.com>
2017-03-12 20:37:30 -0500

    Code cleanup, add view buttons

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
 window.onload = function(){
-    //variable Assigning
-    var addTaskButton = document.getElementById("addTaskButton");
-    var addTaskInput = document.getElementById("addTaskInput");
-
-    //Show task text input from button click
-    addTaskButton.onclick = function() {
-        addTaskInput.classList.toggle("hidden");
-        this.classList.toggle("hidden");
-    };
+    
+   //Show task text input from button click
+    $("#addTaskButton").onclick = toggleFooter;
      
     //Take text input value and add as a new task
-    addTaskInput.addEventListener("keydown", function(e){
+    $("#addTaskInput").addEventListener("keydown", function(e){
         if (event.keyCode == 13) {        
             if (this.value != "") {
                 addTask(this.value);
             }
             this.value = "";
-            this.classList.toggle("hidden")
-            addTaskButton.classList.toggle("hidden");
+            toggleFooter();
+
         }
     });   
 };
 
 //Add Task to List
 function addTask(val) {
-    var tasks = document.getElementById("tasks");
     var taskBar=document.createElement("div");
     taskBar.classList.add("task", "bar");
-    var newTask = tasks.appendChild(taskBar);
+    var newTask = $("#tasks").appendChild(taskBar);
     newTask.innerHTML = "<div class='circle' onclick='addCheck(this)'>\
                             <i class='fa fa-check check hidden' aria-hidden='true'></i>\
                         </div>\
                         <span>"+val+"</span>\
                         <div class='close' onclick='removeParent(this)'>x</div>";
-}
+};
 
 //Add Check Mark
 function addCheck(ele) {
     var checkMark = ele.querySelector("i");
     checkMark.classList.toggle("hidden");
-}
+};
 
 //Remove Parrent Element   
 function removeParent(ele) {
     ele.parentElement.remove();
 };
 
+//jquery like selector
+function $(selector){
+    return document.querySelector(selector);
+};
 
+//First argument is the class you want to toggle, then as many selectors that you want to affect
+function toggleClass(className) {
+    for (var i=1; i<arguments.length; i++) {
+        $(arguments[i]).classList.toggle(className);
+    }
+};
+
+//Toggle footer elmenet to be hidden or unhidden
+function toggleFooter() {
+    toggleClass("hidden", "#addTaskButton", "#addTaskInput", "#showAll", "#showComplete", "#showIncomplete");
+};
